c09a025a68fb4d295085c903f08533f5bb6772cd
Memoona Nasir <Memoona Nasir@LAPTOP-L1OU13J0>
2022-09-30 21:27:26 +0100

    Add project files.

---
+++ ExportStudentsData/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExportStudentsData.Models;
+using Rotativa;
+
+namespace ExportStudentsData.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        StudentDBContext studentDBContext=new StudentDBContext();
+        public ActionResult Index()
+        {
+            return View(studentDBContext.Students.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student oStudent)
+        {
+            try { 
+
+                studentDBContext.Students.Add(oStudent);
+                studentDBContext.SaveChanges();
+                }
+            catch
+            {
+                throw;
+            }
+
+            return View();
+        }
+        public ActionResult PrintViewToPdf()
+        {
+            var report = new ActionAsPdf("Index");
+            return report;
+        }
+
+
+
+        public ActionResult PrintPartialViewToPdf(int id)
+        {
+            using (StudentDBContext db = new StudentDBContext())
+            {
+                Student oStudent = db.Students.FirstOrDefault(s => s.StudentID == id);
+
+                var report = new PartialViewAsPdf("~/Views/Shared/DetailStudent.cshtml", oStudent);
+                return report;
+            }
+
+        }
+
+    }
+}
---
+++ ExportStudentsData/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExportStudentsData.Models
+{
+    public class Student
+    {
+        public int StudentID { set; get; }
+        [Display(Name ="Student Name")]
+        [Required(ErrorMessage ="Student name is required")]
+        public string StudentName { set; get; }
+        [Display(Name = "Father Name")]
+        [Required(ErrorMessage ="Father name is required")]
+        public string FatherName { set; get; }
+        [Display(Name = "Course")]
+        public string CourseName { set; get; }
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { set; get; }
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Email address is invalid")]
+        public string Email { set; get; }
+        [Required(ErrorMessage ="Adress is required")]
+        public string Adrress { set; get; }
+    }
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
